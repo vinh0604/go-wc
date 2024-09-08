d2a71f148fa74d56ef32bea42408d02928fec9fa
Vinh Bach <886778+vinh0604@users.noreply.github.com>
2024-09-08 17:08:51 +0000

    re-implement count with single full scan on reader

--- cmd/go-wc/go-wc.go
+++ cmd/go-wc/go-wc.go
@@ -1,12 +1,12 @@
 package main
 
 import (
-	"bufio"
 	"bytes"
 	"flag"
 	"fmt"
 	"io"
 	"os"
+	"unicode"
 )
 
 type countFlags struct {
@@ -14,6 +14,103 @@ type countFlags struct {
 	lines  bool
 	words  bool
 	locale bool
+}
+
+type countBuffer struct {
+	byteCount   int
+	lineCount   int
+	wordCount   int
+	localeCount int
+	lineBuf     []byte
+	wordBuf     []byte
+	localeBuf   []byte
+}
+
+func (cBuf *countBuffer) countLines(buf []byte, n int) {
+	if n == 0 {
+		return
+	}
+
+	lineBuf := append(cBuf.lineBuf, buf[:n]...)
+	lines := bytes.Split(lineBuf, []byte{'\n'})
+	cBuf.lineCount += len(lines) - 1
+	cBuf.lineBuf = lines[len(lines)-1]
+}
+
+func (cBuf *countBuffer) countWords(buf []byte, n int) {
+	wordBuf := append(cBuf.wordBuf, buf[:n]...)
+	words := bytes.Fields(wordBuf)
+	runes := bytes.Runes(wordBuf)
+	if unicode.IsSpace(runes[len(runes)-1]) {
+		cBuf.wordCount += len(words)
+		cBuf.wordBuf = nil
+	} else {
+		cBuf.wordCount += len(words) - 1
+		cBuf.wordBuf = words[len(words)-1]
+	}
+}
+
+func (cBuf *countBuffer) countLocaleChars(buf []byte, n int) {
+	localeBuf := append(cBuf.localeBuf, buf[:n]...)
+	runes := bytes.Runes(localeBuf)
+	cBuf.localeCount += len(runes) - 1
+	cBuf.localeBuf = []byte(string(runes[len(runes)-1]))
+}
+
+func count(r io.Reader, cFlags countFlags) ([]int, error) {
+	cBuf := countBuffer{}
+
+	buf := make([]byte, 4096)
+	for {
+		n, err := r.Read(buf)
+		if err == io.EOF {
+			if cFlags.lines && len(cBuf.lineBuf) > 0 {
+				cBuf.lineCount++
+			}
+			if cFlags.words && len(cBuf.wordBuf) > 0 {
+				cBuf.wordCount++
+			}
+			if cFlags.locale && len(cBuf.localeBuf) > 0 {
+				cBuf.localeCount++
+			}
+			break
+		}
+		if err != nil {
+			return nil, err
+		}
+
+		if cFlags.bytes {
+			cBuf.byteCount += n
+		}
+
+		if cFlags.lines {
+			cBuf.countLines(buf, n)
+		}
+
+		if cFlags.words {
+			cBuf.countWords(buf, n)
+		}
+
+		if cFlags.locale {
+			cBuf.countLocaleChars(buf, n)
+		}
+
+	}
+
+	result := make([]int, 0, 4)
+	if cFlags.bytes {
+		result = append(result, cBuf.byteCount)
+	}
+	if cFlags.lines {
+		result = append(result, cBuf.lineCount)
+	}
+	if cFlags.words {
+		result = append(result, cBuf.wordCount)
+	}
+	if cFlags.locale {
+		result = append(result, cBuf.localeCount)
+	}
+	return result, nil
 }
 
 func main() {
@@ -45,15 +142,14 @@ func main() {
 
 	exitCode := 0
 	if len(args) == 0 {
-		result, err := countForStdIn(cFlags)
+		result, err := count(os.Stdin, cFlags)
 		if err != nil {
 			println(fmt.Sprintf("go-wc: %s", err))
 			exitCode = 1
 		} else {
-			printResult("", result, false)
+			printResult("", result)
 		}
 	} else {
-		showFilename := len(args) > 1
 		for _, arg := range args {
 			result, err := countForFile(arg, cFlags)
 
@@ -61,7 +157,7 @@ func main() {
 				println(fmt.Sprintf("go-wc: %s: %s", arg, err))
 				exitCode = 1
 			} else {
-				printResult(arg, result, showFilename)
+				printResult(arg, result)
 			}
 		}
 	}
@@ -69,19 +165,18 @@ func main() {
 	os.Exit(exitCode)
 }
 
-func printResult(arg string, result []int, showFilename bool) {
-	if len(result) > 1 {
-		for _, cnt := range result {
-			print(fmt.Sprintf("\t%d", cnt))
+func printResult(arg string, result []int) {
+	for i, cnt := range result {
+		if i > 0 {
+			print(" ")
 		}
-	} else if len(result) == 1 {
-		print(result[0])
+		print(cnt)
 	}
 
-	if showFilename {
-		print(fmt.Sprintf("\t%s", arg))
+	if arg != "" {
+		print(" ")
+		print(arg)
 	}
-
 	print("\n")
 }
 
@@ -92,107 +187,5 @@ func countForFile(file string, flags countFlags) ([]int, error) {
 	}
 	defer fileReader.Close()
 
-	result := make([]int, 0, 4)
-
-	if flags.bytes {
-		fileReader.Seek(0, 0)
-		result = append(result, countBytes(fileReader))
-	}
-
-	if flags.lines {
-		fileReader.Seek(0, 0)
-		result = append(result, countLines(fileReader))
-	}
-
-	if flags.words {
-		fileReader.Seek(0, 0)
-		result = append(result, countWords(fileReader))
-	}
-
-	if flags.locale {
-		fileReader.Seek(0, 0)
-		result = append(result, countLocaleChars(fileReader))
-	}
-
-	return result, nil
-}
-
-func countForStdIn(flags countFlags) ([]int, error) {
-	var buffer bytes.Buffer
-	// Copy from stdin to buffer
-	// This is done to be able to read the StdIn multiple times
-	// TODO: Implement multple count flags from a single read
-	_, err := io.Copy(&buffer, os.Stdin)
-	if err != nil {
-		return nil, err
-	}
-
-	result := make([]int, 0, 4)
-
-	if flags.bytes {
-		result = append(result, countBytes(bytes.NewReader(buffer.Bytes())))
-	}
-
-	if flags.lines {
-		result = append(result, countLines(bytes.NewReader(buffer.Bytes())))
-	}
-
-	if flags.words {
-		result = append(result, countWords(bytes.NewReader(buffer.Bytes())))
-	}
-
-	if flags.locale {
-		result = append(result, countLocaleChars(bytes.NewReader(buffer.Bytes())))
-	}
-
-	return result, nil
-}
-
-func countBytes(r io.Reader) int {
-	buf := make([]byte, 4096)
-	total := 0
-	for {
-		n, err := r.Read(buf)
-		total += n
-		if err == io.EOF {
-			break
-		}
-	}
-
-	return total
-}
-
-func countLines(r io.Reader) int {
-	scanner := bufio.NewScanner(r)
-	total := 0
-
-	for scanner.Scan() {
-		total++
-	}
-
-	return total
-}
-
-func countWords(r io.Reader) int {
-	scanner := bufio.NewScanner(r)
-	scanner.Split(bufio.ScanWords)
-	total := 0
-
-	for scanner.Scan() {
-		total++
-	}
-
-	return total
-}
-
-func countLocaleChars(r io.Reader) int {
-	scanner := bufio.NewScanner(r)
-	scanner.Split(bufio.ScanRunes)
-	total := 0
-
-	for scanner.Scan() {
-		total++
-	}
-
-	return total
+	return count(fileReader, flags)
 }
--- cmd/go-wc/go-wc_test.go
+++ cmd/go-wc/go-wc_test.go
@@ -2,81 +2,8 @@ package main
 
 import (
 	"slices"
-	"strings"
 	"testing"
 )
-
-func TestCountBytes(t *testing.T) {
-	if countBytes(strings.NewReader("hello")) != 5 {
-		t.Error("countBytes should return 5 for 'hello'")
-	}
-
-	if countBytes(strings.NewReader("hello\nworld")) != 11 {
-		t.Error("countBytes should return 11 for 'hello\nworld'")
-	}
-
-	if countBytes(strings.NewReader("hêllo")) != 6 {
-		t.Error("countBytes should return 6 for 'hêllo'")
-	}
-}
-
-func TestCountLines(t *testing.T) {
-	if countLines(strings.NewReader("hello")) != 1 {
-		t.Error("countLines should return 1 for 'hello'")
-	}
-
-	if countLines(strings.NewReader("hello\nworld")) != 2 {
-		t.Error("countLines should return 2 for 'hello\nworld'")
-	}
-
-	if countLines(strings.NewReader("hello\r\nworld")) != 2 {
-		t.Error("countLines should return 2 for 'hello\r\nworld'")
-	}
-}
-
-func TestCountWords(t *testing.T) {
-	if countWords(strings.NewReader("hello")) != 1 {
-		t.Error("countWords should return 1 for 'hello'")
-	}
-
-	if countWords(strings.NewReader("hello world")) != 2 {
-		t.Error("countWords should return 2 for 'hello world'")
-	}
-
-	if countWords(strings.NewReader("hello  world")) != 2 {
-		t.Error("countWords should return 2 for 'hello  world'")
-	}
-
-	if countWords(strings.NewReader("\thello  world")) != 2 {
-		t.Error("countWords should return 2 for '\thello  world'")
-	}
-
-	if countWords(strings.NewReader("\thello \n world")) != 2 {
-		t.Error("countWords should return 2 for '\thello \n world'")
-	}
-
-	if countWords(strings.NewReader("")) != 0 {
-		t.Error("countWords should return 0 for ''")
-	}
-
-	if countWords(strings.NewReader("\t\n ")) != 0 {
-		t.Error("countWords should return 0 for '\t\n '")
-	}
-}
-
-func TestCountLocaleChars(t *testing.T) {
-	if countLocaleChars(strings.NewReader("hello")) != 5 {
-		t.Error("countLocaleChars should return 5 for 'hello'")
-	}
-
-	if countLocaleChars(strings.NewReader("hello\nworld")) != 11 {
-		t.Error("countLocaleChars should return 11 for 'hello\nworld'")
-	}
-
-	if countLocaleChars(strings.NewReader("hêllo")) != 5 {
-		t.Error("countLocaleChars should return 5 for 'hêllo'")
-	}
-}
 
 func TestCountForFile(t *testing.T) {
 	res, err := countForFile("../../data/test.txt", countFlags{
@@ -93,5 +20,102 @@ func TestCountForFile(t *testing.T) {
 	expected := []int{335042, 7145, 58164, 332146}
 	if !slices.Equal(res, expected) {
 		t.Errorf("countForFile should return %v. Got: %v", expected, res)
+	}
+}
+
+func TestCountBufferCountLines(t *testing.T) {
+	cBuf := countBuffer{}
+	cBuf.countLines([]byte("hello\nwo"), 8)
+	if cBuf.lineCount != 1 {
+		t.Error("countLines should increase 1 for 'hello\\nwo', got:", cBuf.lineCount)
+	}
+	if !slices.Equal(cBuf.lineBuf, []byte("wo")) {
+		t.Errorf("countLines should set lineBuf to 'wo', got: %v", cBuf.lineBuf)
+	}
+
+	cBuf.countLines([]byte("rld\n"), 4)
+	if cBuf.lineCount != 2 {
+		t.Error("countLines should increase 1 for 'rld\\n', got:", cBuf.lineCount)
+	}
+	if !slices.Equal(cBuf.lineBuf, []byte{}) {
+		t.Errorf("countLines should set lineBuf to nil, got: %v", cBuf.lineBuf)
+	}
+
+	cBuf.countLines([]byte("\n"), 1)
+	if cBuf.lineCount != 3 {
+		t.Error("countLines should increase 1 for '\\n', got:", cBuf.lineCount)
+	}
+	if !slices.Equal(cBuf.lineBuf, []byte{}) {
+		t.Errorf("countLines should set lineBuf to nil, got: %v", cBuf.lineBuf)
+	}
+}
+
+func TestCountBufferCountWords(t *testing.T) {
+	cBuf := countBuffer{}
+	cBuf.countWords([]byte("hello"), 5)
+	if cBuf.wordCount != 0 {
+		t.Error("countWords should increase 0 for 'hello', got:", cBuf.wordCount)
+	}
+	if !slices.Equal(cBuf.wordBuf, []byte("hello")) {
+		t.Errorf("countWords should set wordBuf to 'hello', got: %v", cBuf.wordBuf)
+	}
+
+	cBuf.countWords([]byte("  world"), 7)
+	if cBuf.wordCount != 1 {
+		t.Error("countWords should increase 1 for '  world', got:", cBuf.wordCount)
+	}
+	if !slices.Equal(cBuf.wordBuf, []byte("world")) {
+		t.Errorf("countWords should set wordBuf to 'world', got: %v", cBuf.wordBuf)
+	}
+
+	cBuf.countWords([]byte("\t\n hello "), 9)
+	if cBuf.wordCount != 3 {
+		t.Error("countWords should increase 2 for '\\t\\n hello ', got:", cBuf.wordCount)
+	}
+	if !slices.Equal(cBuf.wordBuf, []byte{}) {
+		t.Errorf("countWords should set wordBuf to nil, got: %v", cBuf.wordBuf)
+	}
+
+	cBuf.countWords([]byte("world \n\n"), 8)
+	if cBuf.wordCount != 4 {
+		t.Error("countWords should increase 1 for 'world \\n\\n', got:", cBuf.wordCount)
+	}
+	if !slices.Equal(cBuf.wordBuf, []byte{}) {
+		t.Errorf("countWords should set wordBuf to nil, got: %v", cBuf.wordBuf)
+	}
+
+	cBuf.countWords([]byte("hel"), 3)
+	if cBuf.wordCount != 4 {
+		t.Error("countWords should increase 1 for 'hel', got:", cBuf.wordCount)
+	}
+	if !slices.Equal(cBuf.wordBuf, []byte("hel")) {
+		t.Errorf("countWords should set wordBuf to 'hel', got: %v", cBuf.wordBuf)
+	}
+
+	cBuf.countWords([]byte("lo"), 2)
+	if cBuf.wordCount != 4 {
+		t.Error("countWords should increase 0 for 'lo', got:", cBuf.wordCount)
+	}
+	if !slices.Equal(cBuf.wordBuf, []byte("hello")) {
+		t.Errorf("countWords should set wordBuf to 'hello', got: %v", cBuf.wordBuf)
+	}
+}
+
+func TestCountBufferCountLocaleChars(t *testing.T) {
+	cBuf := countBuffer{}
+	cBuf.countLocaleChars([]byte("hello "), 6)
+	if cBuf.localeCount != 5 {
+		t.Error("countLocaleChars should increase 5 for 'hello ', got:", cBuf.localeCount)
+	}
+	if !slices.Equal(cBuf.localeBuf, []byte(" ")) {
+		t.Errorf("countLocaleChars should set localeBuf to ' ', got: %v", cBuf.localeBuf)
+	}
+
+	cBuf.countLocaleChars([]byte("wôrld"), 6)
+	if cBuf.localeCount != 10 {
+		t.Error("countLocaleChars should increase 5 for 'wôrld', got:", cBuf.localeCount)
+	}
+	if !slices.Equal(cBuf.localeBuf, []byte{'d'}) {
+		t.Errorf("countLocaleChars should set localeBuf to 'd', got: %v", cBuf.localeBuf)
 	}
 }
